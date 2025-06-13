541774f8df6311dabf9664d4a30bc51732208038
elysianiu_i <elysianiu_i@didiglobal.com>
2025-06-13 13:53:09 +0800

    Upload New File

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'order',
+    version = '0.1.0',
+    description = 'order_fast',
+    author = 'elysianiu_i',
+    author_email = 'elysianiu_i@didiglobal.com',
+    url = "https://git.xiaojukeji.com/dmq/order_mcp_server",
+    packages = find_packages(),
+    python_requires = '>=3.6',
+    zip_safe = False
+)
