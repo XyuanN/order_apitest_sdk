97c36645e1e3b02e35c48314788f031f686b4b5a
elysianiu_i <elysianiu_i@didiglobal.com>
2025-06-16 15:46:03 +0800

    Update client.py

--- orderflow/client.py
+++ orderflow/client.py
@@ -7,8 +7,11 @@ from orders.fast import OrderFlowFast
 
 if __name__ == '__main__':
     phone = sys.stdin.readline().strip()
+    # 创建订单
     hailing_client = OrderFlowFast()
     variables = hailing_client.create_order(phone)
+    
+    # 订单流转
     hailing_client.accept_order(variables)
     hailing_client.driver_arrived(variables)
     hailing_client.start_trip(variables)
